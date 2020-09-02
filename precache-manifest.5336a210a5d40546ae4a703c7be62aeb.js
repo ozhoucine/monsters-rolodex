4e6afeebd8e75653926a54f76356f1e58a06c8b8
ozhoucine <ouziz404@gmail.com>
2020-09-02 19:32:35 +0000

    Updates

---
+++ precache-manifest.5336a210a5d40546ae4a703c7be62aeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5da81c88d5e1ec1645a31b6d11742e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5494dfa600d17d3a2f4c",
+    "url": "/monsters-rolodex/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "e9680ee62bee18642f9b",
+    "url": "/monsters-rolodex/static/js/2.9b83ecc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9b83ecc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5494dfa600d17d3a2f4c",
+    "url": "/monsters-rolodex/static/js/main.27b63a11.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/monsters-rolodex/static/media/logo.ee7cd8ed.svg"
+  }
+]);
